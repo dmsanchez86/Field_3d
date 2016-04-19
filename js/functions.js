36333bac0b6617611979a1d872947d0ce1775fb6
Daniel M. Sanchez <dmsanchez86@misena.edu.co>
2016-04-19 12:12:19 -0500

    add cameras, add styles to controls ccamera

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,44 @@
 'use strict';
 
 let App = {
+	field: null,
+	toggle: true,
+
 	init: function(){
+		this.field = document.querySelector('.field');
 		this.controlsCameraFunction();
 	},
 	controlsCameraFunction: function(){
-		var camerasItems  = document.querySelectorAll(".controlsCamera > span");
+		var camerasItems  = document.querySelectorAll(".controlsCamera > .control");
 
 		for (var i = 0; i < camerasItems.length; i++) {
 			camerasItems[i].onclick = function(e){
-				console.log(e.target.classList);
-				console.log(this);
+				var cameraPerspective = this.getAttribute('camera');
+
+				if(cameraPerspective == "normal"){
+					App.field.className = "field";
+				}else if(cameraPerspective == "front"){
+					App.field.className = "field viewer front";
+				}else if(cameraPerspective == "back"){
+					App.field.className = "field viewer back";
+				}else if(cameraPerspective == "side_left"){
+					App.field.className = "field viewer side_left";
+				}else if(cameraPerspective == "side_right"){
+					App.field.className = "field viewer side_right";
+				}
+
+				// if(App.toggle){
+				// 	App.field.className = "field viewer";
+				// 	App.toggle = !App.toggle;
+				// }else{
+				// 	App.field.className = "field";
+				// 	App.toggle = !App.toggle;
+				// }
 			}
 		}
 	}
 };
 
 window.onload = function(){
-App.init();
+	App.init();
 }
