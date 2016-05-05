631d07194d9588fcfedea256471ff432b6685476
Daniel M. Sánchez <dmsanchez86@misena.edu.co>
2016-05-05 22:27:00 +0000

    add key events

--- js/functions.js
+++ js/functions.js
@@ -9,35 +9,32 @@ let App = {
 		this.controlsCameraFunction();
 		// this.getLocalTeam();
 		this.movePlayers();
+		this.windowPress();
 	},
 	controlsCameraFunction: function(){
-		var camerasItems  = document.querySelectorAll(".controlsCamera > .control");
+		$('.controlsCamera > .control').click(function(e){
+			var cameraPerspective = $(this).attr('camera');
 
-		for (var i = 0; i < camerasItems.length; i++) {
-			camerasItems[i].onclick = function(e){
-				var cameraPerspective = this.getAttribute('camera');
-
-				if(cameraPerspective == "normal"){
-					App.field.className = "field";
-				}else if(cameraPerspective == "front"){
-					App.field.className = "field viewer front";
-				}else if(cameraPerspective == "back"){
-					App.field.className = "field viewer back";
-				}else if(cameraPerspective == "side_left"){
-					App.field.className = "field viewer side_left";
-				}else if(cameraPerspective == "side_right"){
-					App.field.className = "field viewer side_right";
-				}
-
-				// if(App.toggle){
-				// 	App.field.className = "field viewer";
-				// 	App.toggle = !App.toggle;
-				// }else{
-				// 	App.field.className = "field";
-				// 	App.toggle = !App.toggle;
-				// }
+			if(cameraPerspective == "normal"){
+				App.field.className = "field";
+			}else if(cameraPerspective == "front"){
+				App.field.className = "field viewer front";
+			}else if(cameraPerspective == "back"){
+				App.field.className = "field viewer back";
+			}else if(cameraPerspective == "side_left"){
+				App.field.className = "field viewer side_left";
+			}else if(cameraPerspective == "side_right"){
+				App.field.className = "field viewer side_right";
 			}
-		}
+
+			// if(App.toggle){
+			// 	App.field.className = "field viewer";
+			// 	App.toggle = !App.toggle;
+			// }else{
+			// 	App.field.className = "field";
+			// 	App.toggle = !App.toggle;
+			// }
+		});
 	},
 	
 	getLocalTeam: function(){
@@ -74,7 +71,26 @@ let App = {
 		      $(event.target).removeClass("active");
 		  	}
 		});
-	}
+	},
+	
+	// event window when press keyboards
+	windowPress: function(){
+		$(document).keydown(function(event){
+			var code = event.keyCode; // get the code
+			console.log(code);
+
+			if(code == 49) // num 1
+				$('.controlsCamera > .control').eq(0).click();
+			else if(code == 50) // num 2
+				$('.controlsCamera > .control').eq(1).click();
+			else if(code == 51) // num 3
+				$('.controlsCamera > .control').eq(2).click();
+			else if(code == 52) // num 4
+				$('.controlsCamera > .control').eq(3).click();
+			else if(code == 53) // num 5
+				$('.controlsCamera > .control').eq(4).click();
+		});
+	},
 };
 
 window.onload = function(){
