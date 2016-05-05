d026edd365bcbeaf7f4098ca570aca7a2381a3f1
Daniel M. Sánchez <dmsanchez86@misena.edu.co>
2016-05-05 22:04:04 +0000

    add favicon, new crossbar, background images to players

--- js/functions.js
+++ js/functions.js
@@ -80,34 +80,3 @@ let App = {
 window.onload = function(){
 	App.init();
 }
-
-!function() {
-    "use strict";
-    var $Godo = $(".fiel"),
-        $Scene = $("main"),
-        axis = {
-            x: 0,
-            y: 0,
-            rx: 0,
-            ry: 0,
-            rz: 0,
-            s: 1
-        },
-        GodoScene = new Hammer.Manager($Scene[0]);
-    	GodoScene.add(new Hammer.Pan({
-        direction: Hammer.DIRECTION_ALL,
-        threshold: 0
-    })), GodoScene.add(new Hammer.Pinch({
-        threshold: 0
-    })), GodoScene.on("pan", function(e) {
-        axis.rx = axis.x + e.deltaX, axis.ry = axis.y - e.deltaY, TweenMax.to($Godo, .5, {
-            scale: axis.s,
-            rotationY: axis.rx,
-            rotationX: axis.ry,
-            overwrite: 1,
-            ease: Strong.easeOut
-        })
-    }).on("panend", function(e) {
-        axis.x += e.deltaX, axis.y -= e.deltaY
-    })
-}();
