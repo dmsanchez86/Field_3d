12948db1a1002bf13681b83a6043900fef639fab
Daniel M. Sánchez <dmsanchez86@misena.edu.co>
2016-07-07 18:21:33 -0500

    added db teams 50%

--- js/config.js
+++ js/config.js
@@ -1,5 +1,14 @@
 var application = angular.module('fieldFootball', []);
 
-application.controller('mainCtrl', function($scope, $window){
-  // alert();
+application.controller('mainCtrl', function($scope, $window, $http){
+  $scope.db = [];
+  $scope.localTeam = [];
+  $scope.visitantTeam = [];
+
+  $http.get('js/db.json').success(function(response) {
+      $scope.db = response.db;
+      console.log($scope.db);
+      $scope.localTeam = $scope.db[0];
+      console.log($scope.localTeam);
+  });
 });
