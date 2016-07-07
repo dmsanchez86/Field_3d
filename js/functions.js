8e48fca6fab54ecb987fabb1a005044070549467
Daniel M. Sánchez <dmsanchez86@misena.edu.co>
2016-07-07 08:26:35 -0500

    updated code to keydown event

--- js/functions.js
+++ js/functions.js
@@ -87,15 +87,15 @@ let App = {
 			console.log(code);
 
 			if(code == 49) // num 1
-				$('.controlsCamera > .control').eq(0).click();
+				$('.sideField').eq(0).click();
 			else if(code == 50) // num 2
-				$('.controlsCamera > .control').eq(1).click();
+				$('.sideField').eq(1).click();
 			else if(code == 51) // num 3
-				$('.controlsCamera > .control').eq(2).click();
+				$('.sideField').eq(2).click();
 			else if(code == 52) // num 4
-				$('.controlsCamera > .control').eq(3).click();
+				$('.sideField').eq(3).click();
 			else if(code == 53) // num 5
-				$('.controlsCamera > .control').eq(4).click();
+				$('.cameraField').click();
 		});
 	},
 
